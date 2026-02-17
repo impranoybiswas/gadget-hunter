db26c679348cec783274144ca43d0612ab61b24a
Pranoy Biswas Bappa <75092679+impranoybiswas@users.noreply.github.com>
2026-02-17 13:59:06 +0600

    update : connectDB structure changed

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,15 @@
 import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function middleware(req : NextRequest) {
+export async function middleware(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-  const url = req.nextUrl.pathname;
 
   // If not logged in, redirect to login
   if (!token) {
     return NextResponse.redirect(new URL("/auth/login", req.url));
   }
 
-  const role = token.role?.toLowerCase();
-
   // Not Allowed
-  
 }
 
 export const config = {
--- src/libs/connectDB.ts
+++ src/libs/connectDB.ts
@@ -1,11 +1,47 @@
-import clientPromise from "./mongodb";
+import { MongoClient, ServerApiVersion } from "mongodb";
 
-const databaseName = process.env.DATABASE_NAME || "";
+if (!process.env.MONGODB_URI) {
+  throw new Error("Missing MONGODB_URI");
+}
+
+if (!process.env.DATABASE_NAME) {
+  throw new Error("Missing DATABASE_NAME");
+}
+
+const uri = process.env.MONGODB_URI;
+
+const options = {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+};
+
+type GlobalMongo = typeof globalThis & {
+  _mongoClientPromise?: Promise<MongoClient>;
+  _mongoClientUri?: string;
+};
+
+const globalWithMongo = global as GlobalMongo;
+
+if (
+  !globalWithMongo._mongoClientPromise ||
+  globalWithMongo._mongoClientUri !== uri
+) {
+  const client = new MongoClient(uri, options);
+  globalWithMongo._mongoClientPromise = client.connect();
+  globalWithMongo._mongoClientUri = uri;
+}
+
+const clientPromise = globalWithMongo._mongoClientPromise!;
+
+const databaseName = process.env.DATABASE_NAME;
 
 async function connectDB(dbName: string) {
   try {
     const client = await clientPromise;
-    console.log(">>>>> Connected to MongoDB (Shared Client) <<<<<");
+    console.log(">>>>> Connected to MongoDB <<<<<");
     return client.db(dbName);
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
